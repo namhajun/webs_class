e315ff2c71d8d62fc021586fb5d78bc79722ac28
GarenChoi <97070058+songjunseop@users.noreply.github.com>
2022-01-18 18:09:51 +0900

    f

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -1,50 +1,50 @@
-//결과보기
-//제목을클릭했을때 ~~을 실행하라
+// //결과보기
+// //제목을클릭했을때 ~~을 실행하라
 
-document.querySelector(".result01t h4").addEventListener("click", function () {
-    document.querySelector(".result01").style.display = "block";
-})
+// document.querySelector(".result1t h4").addEventListener("click", function () {
+//     document.querySelector(".result1").style.display = "block";
+// })
 
-document.querySelector(".result02t h4").addEventListener("click", function () {
-    document.querySelector(".result02").style.display = "block";
-})
-document.querySelector(".result03t h4").addEventListener("click", function () {
-    document.querySelector(".result03").style.display = "block";
-})
-document.querySelector(".result04t h4").addEventListener("click", function () {
-    document.querySelector(".result04").style.display = "block";
-})
-document.querySelector(".result05t h4").addEventListener("click", function () {
-    document.querySelector(".result05").style.display = "block";
-})
-document.querySelector(".result06t h4").addEventListener("click", function () {
-    document.querySelector(".result06").style.display = "block";
-})
-document.querySelector(".result07t h4").addEventListener("click", function () {
-    document.querySelector(".result07").style.display = "block";
-})
+// document.querySelector(".result2t h4").addEventListener("click", function () {
+//     document.querySelector(".result2").style.display = "block";
+// })
+// document.querySelector(".result3t h4").addEventListener("click", function () {
+//     document.querySelector(".result3").style.display = "block";
+// })
+// document.querySelector(".result4t h4").addEventListener("click", function () {
+//     document.querySelector(".result4").style.display = "block";
+// })
+// document.querySelector(".result5t h4").addEventListener("click", function () {
+//     document.querySelector(".result5").style.display = "block";
+// })
+// document.querySelector(".result6t h4").addEventListener("click", function () {
+//     document.querySelector(".result6").style.display = "block";
+// })
+// document.querySelector(".result7t h4").addEventListener("click", function () {
+//     document.querySelector(".result7").style.display = "block";
+// })
 
-document.querySelector(".result08t h4").addEventListener("click", function () {
-    document.querySelector(".result08").style.display = "block";
-})
-document.querySelector(".result09t h4").addEventListener("click", function () {
-    document.querySelector(".result09").style.display = "block";
-})
-document.querySelector(".result10t h4").addEventListener("click", function () {
-    document.querySelector(".result10").style.display = "block";
-})
-document.querySelector(".result11t h4").addEventListener("click", function () {
-    document.querySelector(".result11").style.display = "block";
-})
-document.querySelector(".result12t h4").addEventListener("click", function () {
-    document.querySelector(".result12").style.display = "block";
-})
-document.querySelector(".result13t h4").addEventListener("click", function () {
-    document.querySelector(".result13").style.display = "block";
-})
-document.querySelector(".result14t h4").addEventListener("click", function () {
-    document.querySelector(".result14").style.display = "block";
-})
+// document.querySelector(".result8t h4").addEventListener("click", function () {
+//     document.querySelector(".result8").style.display = "block";
+// })
+// document.querySelector(".result9t h4").addEventListener("click", function () {
+//     document.querySelector(".result9").style.display = "block";
+// })
+// document.querySelector(".result1t h4").addEventListener("click", function () {
+//     document.querySelector(".result1").style.display = "block";
+// })
+// document.querySelector(".result11t h4").addEventListener("click", function () {
+//     document.querySelector(".result11").style.display = "block";
+// })
+// document.querySelector(".result12t h4").addEventListener("click", function () {
+//     document.querySelector(".result12").style.display = "block";
+// })
+// document.querySelector(".result13t h4").addEventListener("click", function () {
+//     document.querySelector(".result13").style.display = "block";
+// })
+// document.querySelector(".result14t h4").addEventListener("click", function () {
+//     document.querySelector(".result14").style.display = "block";
+// })
 
 
 
@@ -87,3 +87,9 @@ document.querySelector(".result14t h4").addEventListener("click", function () {
 //     console.log(el) //100 200 300 400 500
 //     console.log(index) // 0 1 2 3 4
 // });
+//결과보기 버튼을 클릭하면 경고창을 띄어 주세요
+document.querySelectorAll(".result").forEach(function(el){
+  addEventListener("click", function(){
+    alert("ddd")
+});
+})
