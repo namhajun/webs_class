913baaab4af83ac3e8eb0488eda9e59a85ee6912
namhajun <97070067+namhajun@users.noreply.github.com>
2022-04-07 18:31:50 +0900

    vffgff

---
+++ musicplayer/api/music.js
@@ -0,0 +1,80 @@
+let allMusic = [
+    {
+        name:"80's Vampire Car Chase",
+        artist: "Sir Cubworth",
+        img:"music-1",
+        src:"./images/music-1"
+    },
+    {
+        name:"Brain Trust ",
+        artist: " Wayne Jones",
+        img:"music-2",
+        src:"music-2"
+    },
+    {
+        name:"Erev Shel Shoshanim - E's Jammy Jams",
+        artist: " E's Jammy Jams",
+        img:"music-3",
+        src:"music-3"
+    },
+    {
+        name:"Floating Home - Brian Bolger",
+        artist: " Brian Bolger",
+        img:"music-4",
+        src:"music-4"
+    },
+    {
+        name:"In Memory of Jean Talon - The Mini Vandals",
+        artist: "The Mini Vandals",
+        img:"music-5",
+        src:"music-5"
+    },
+    {
+        name:"Ocean View - Patrick Patrikios",
+        artist: "Patrick Patrikios",
+        img:"music-6",
+        src:"music-6"
+    },
+    {
+        name:"Polaroid Memories - DivKid",
+        artist: "DivKid",
+        img:"music-7",
+        src:"music-7"
+    },
+    {
+        name:"Sunday - TrackTribe",
+        artist: " TrackTribe",
+        img:"music-8",
+        src:"music-8"
+    },
+    {
+        name:"Turbo Stasis - Sir Cubworth",
+        artist: "Sir Cubworth",
+        img:"music-9",
+        src:"music-9"
+    },
+    {
+        name:"Yah Yah  ",
+        artist: "josh pan",
+        img:"music-10",
+        src:"music-10"
+    },
+    {
+        name:"Yismach Chatoni - E's Jammy Jams",
+        artist: " E's Jammy Jams",
+        img:"music-11",
+        src:"music-11"
+    },
+    {
+        name:"Breeze - Telecasted",
+        artist: "Telecasted",
+        img:"music-12",
+        src:"music-12"
+    },
+    {
+        name:"Sailing - Telecasted",
+        artist: "Telecasted",
+        img:"music-13",
+        src:"music-13"
+    },
+]
